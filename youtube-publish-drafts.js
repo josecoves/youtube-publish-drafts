77f88c7b06fa8745cdf5cb566d908fb84a2178e1
Jose Coves <jmbendito@gmail.com>
2023-10-28 12:03:20 -1000

    Public

--- youtube-publish-drafts.js
+++ youtube-publish-drafts.js
@@ -10,7 +10,7 @@
     // ~ PUBLISH CONFIG
     // -----------------------------------------------------------------
     const MADE_FOR_KIDS = false; // true / false;
-    const VISIBILITY = 'Unlisted'; // 'Public' / 'Private' / 'Unlisted'
+    const VISIBILITY = 'Public'; // 'Public' / 'Private' / 'Unlisted'
     // -----------------------------------------------------------------
     // ~ SORT PLAYLIST CONFIG
     // -----------------------------------------------------------------
